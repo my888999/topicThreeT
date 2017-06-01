73a8cc62cac77992a8e80c41c9555806853ea9c6
my88899 <haojiutao@gmail.com>
2017-06-01 16:33:30 +0800

    finish function stringSum

--- main/main.js
+++ main/main.js
@@ -1,3 +1,10 @@
-module.exports = function main() {
+module.exports = function main(tString) {
   // Write your cade here
+  var tNum = Number(tString);
+  var tSum = 0;
+  while(tNum>0){
+  	tSum += tNum % 10;
+  	tNum = Math.floor(tNum / 10);
+  }
+  return tSum;
 };
